9eb467479cdd41c73a1811db8876f93cbc6331e4
Aurelien CAZUC <acazuc@student.42.fr>
2016-01-08 10:18:26 +0100

    Normed

--- srcs/light_level_mask.c
+++ srcs/light_level_mask.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/04 15:53:39 by acazuc            #+#    #+#             */
-/*   Updated: 2016/01/08 07:37:26 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/01/08 10:17:57 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 void		add_mask_smooth(t_color_mask *mask, t_vector *normal_v
 		, t_ray *ray, t_light_collision *data)
 {
+	t_color_mask	*m;
+	t_vector		*nm;
+	t_vector		*rd;
+	double			ll;
+
+	m = data->light->mask;
+	nm = data->collision->normal;
+	rd = ray->direction;
+	ll = data->light->luminosity;
 	if (data->collision->object->type != SPHERE
 			&& data->collision->object->type != CYLINDER
 			&& data->collision->object->type != CONE)
 		return ;
-	mask->red += MAX(0, cos(vector_angle(normal_v, ray->direction))) *
-				data->light->luminosity * data->light->mask->red *
-				MAX(0, 1 - 20 *
-						(1 - sin(vector_angle(ray->direction
-											  , data->collision->normal))));
-	mask->green += MAX(0, cos(vector_angle(normal_v, ray->direction))) *
-				data->light->luminosity * data->light->mask->green *
-				MAX(0, 1 - 20 *
-						(1 - sin(vector_angle(ray->direction
-											  , data->collision->normal))));
-	mask->blue += MAX(0, cos(vector_angle(normal_v, ray->direction))) *
-				data->light->luminosity * data->light->mask->blue *
-				MAX(0, 1 - 20 *
-						(1 - sin(vector_angle(ray->direction
-											  , data->collision->normal))));
+	mask->red += MAX(0, cos(vector_angle(normal_v, rd))) * ll * m->red *
+				MAX(0, 1 - 20 * (1 - sin(vector_angle(rd, nm))));
+	mask->green += MAX(0, cos(vector_angle(normal_v, rd))) * ll * m->green *
+				MAX(0, 1 - 20 * (1 - sin(vector_angle(rd, nm))));
+	mask->blue += MAX(0, cos(vector_angle(normal_v, rd))) * ll * m->blue *
+				MAX(0, 1 - 20 * (1 - sin(vector_angle(rd, nm))));
 }
 
 void		add_mask_specular(t_color_mask *mask, t_ray *ray
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 13:59:40 by acazuc            #+#    #+#             */
-/*   Updated: 2016/01/08 10:02:36 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/01/08 10:13:49 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,15 @@
 
 int		main(void)
 {
-	t_object	*object;
-	t_light		*light;
 	t_env		*env;
 
 	if (!(env = malloc(sizeof(*env))))
 		error_quit("Failed to malloc env");
 	env_init(env);
 	window_init(env);
-	
-	/**
-	 * ROTATIONS
-	 */
 	parse_scene(env, "scenes/demo.rt");
-	/*env->position->z = -20;
-	env->position->y = 20;
-	env->position->x = 0;
-	env->rotation->x = -45;
-	env->rotation->y = 0;*/
-	light = light_create();
-	light->position->x = -5;
-	light->position->z = 0;
-	light->position->y = 10;
-	light->mask->green = 0;
-	light->mask->blue = 0;
-	light->luminosity = 3;
-	/*light_add(env, light);
-	light = light_create();
-	light->position->x = 0;
-	light->position->z = 0;
-	light->position->y = 10;
-	light->mask->red = 0;
-	light->mask->blue = 0;
-	light->luminosity = 3;
-	light_add(env, light);
-	light = light_create();
-	light->position->x = 5;
-	light->position->z = 0;
-	light->position->y = 10;
-	light->mask->red = 0;
-	light->mask->green = 0;
-	light->luminosity = 3;
-	light_add(env, light);*/
-	/*int i = -20;
-	while (i <= -30)
-	{
-		object = create_sphere();
-		object->color = RED;
-		object->position->z = 5;
-		object->position->x = i;
-		object->reflection = 1;
-		object->dimensions[0] = .9;
-		object_add(env, object);
-		object = create_sphere();
-		i += 5;
-	}
-	object->position->y = -10000001;
-	object->color = WHITE;
-	object->dimensions[0] = 10000000;
-	object_add(env, object);*/
-	object = NULL;
 	ft_putstr(CONSOLE_GREEN);
-	ft_putendl("Started drawing");
+	ft_putendl("Scene parsed");
 	draw(env);
 	ft_putstr(CONSOLE_GREEN);
 	ft_putendl("Finished drawing");
