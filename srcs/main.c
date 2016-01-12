6bc5d9c844f895700cb8d23290f96e6824f66b60
Aurelien CAZUC <acazuc@student.42.fr>
2016-01-12 10:53:54 +0100

    Normed

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,21 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 13:59:40 by acazuc            #+#    #+#             */
-/*   Updated: 2016/01/12 09:11:34 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/01/12 10:53:11 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+
+static void		put_time(long start, long end, char *msg)
+{
+	ft_putstr(CONSOLE_GREEN);
+	ft_putstr(msg);
+	ft_putstr(" in ");
+	ft_putnbr(end - start);
+	ft_putendl("ms");
+	ft_putstr(CONSOLE_WHITE);
+}
 
 int		main(int ac, char **av)
 {
@@ -26,20 +36,12 @@ int		main(int ac, char **av)
 	start = epoch_millis();
 	parse_scene(env, av[1]);
 	end = epoch_millis();
-	ft_putstr(CONSOLE_GREEN);
-	ft_putstr("Scene parsed in ");
-	ft_putnbr(end - start);
-	ft_putendl("ms");
-	ft_putstr(CONSOLE_WHITE);
+	put_time(start, end, "Scene parsed");
 	window_init(env);
 	start = epoch_millis();
 	draw(env);
 	end = epoch_millis();
-	ft_putstr(CONSOLE_GREEN);
-	ft_putstr("Finished drawing in ");
-	ft_putnbr(end - start);
-	ft_putendl("ms");
-	ft_putstr(CONSOLE_WHITE);
+	put_time(start, end, "Finished drawing");
 	mlx_expose_hook(env->window->mlx_window, &expose_listener, env);
 	mlx_key_hook(env->window->mlx_window, &key_listener, env);
 	mlx_loop(env->window->mlx);
