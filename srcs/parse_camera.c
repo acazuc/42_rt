f0c34f6fd940c349266ec1ebbc6fc5c9fd707e12
Aurelien CAZUC <acazuc@student.42.fr>
2016-01-30 09:57:53 +0100

    Better camera

--- srcs/parse_camera.c
+++ srcs/parse_camera.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 16:45:06 by acazuc            #+#    #+#             */
-/*   Updated: 2016/01/29 10:48:26 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/01/30 09:57:33 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,11 @@ static void		parse_camera_part(t_env *env, t_parser *p)
 	if (!p->datas[p->count])
 		parse_error(p, "Failed to read camera's param entry");
 	if (!ft_strcmp(p->datas[p->count], "position"))
-	{
-		p->count++;
 		parse_camera_position(env, p);
-	}
 	else if (!ft_strcmp(p->datas[p->count], "rotation"))
-	{
-		p->count++;
 		parse_camera_rotation(env, p);
-	}
 	else if (!ft_strcmp(p->datas[p->count], "fov"))
-	{
-		p->count++;
-		if (!p->datas[p->count] | !parse_valid_number(p->datas[p->count]))
-			parse_error(p, "Failed to read fov");
-		env->fov = ft_atod(p->datas[p->count]);
-	}
+		env->fov = parse_double(p, "Failed to read fov");
 	else
 		parse_error(p, "Unknown cameras's param entry");
 }
