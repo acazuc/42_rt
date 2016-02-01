c06a0f69e3789eccaa9f2626289f3e40be2c113f
Aurelien CAZUC <acazuc@student.42.fr>
2016-02-01 15:33:28 +0100

    Finished obj creation

--- srcs/obj_add_rotate.c
+++ srcs/obj_add_rotate.c
@@ -6,20 +6,20 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 15:24:38 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/01 15:28:32 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/01 15:33:06 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-void	obj_add_rotate(t_obj_file *file, t_obj *obj)
+void	obj_add_rotate(t_obj *obj, t_obj_file *file)
 {
 	t_obj_vertex_list	*lst;
 	
 	lst = file->vertex;
 	while (lst)
 	{
-		vector_rotate(lst, obj->rotation);
+		vector_rotate(lst->vertex, obj->rotation);
 		lst = lst->next;
 	}
 }
