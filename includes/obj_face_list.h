a5065537f129dbfa0ade588d62a82ef5a24bb8f8
Aurelien CAZUC <acazuc@student.42.fr>
2016-02-01 13:33:29 +0100

    Better norm

--- includes/obj_face_list.h
+++ includes/obj_face_list.h
@@ -6,20 +6,20 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 13:29:13 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/01 13:31:44 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/01 13:33:01 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OBJ_FACE_LIST_H
 # define OBJ_FACE_LIST_H
 
-typedef struct		s_obj_face_list t_obj_face_list;
+typedef struct s_obj_face_list	t_obj_face_list;
 
 struct				s_obj_face_list
 {
 	t_vector		*v1;
 	t_vector		*v2;
 	t_vector		*v3;
-}
+};
 
 #endif
